1761f0ca69dd9d2a1af9e055e224255968f6fcc6
Aditya010102 <adityatiw9525@gmail.com>
2026-07-27 03:22:51 +0530

    fixing the apiUrl in dashboard Service

--- frontend/src/app/core/services/dashboard.service.ts
+++ frontend/src/app/core/services/dashboard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -8,7 +9,7 @@ export class DashboardService {
 
     private http = inject(HttpClient);
 
-    private api = 'http://127.0.0.1:5000/api/dashboard';
+    private api = environment.apiUrl
 
     getDashboard() {
         return this.http.get(this.api);
