6d226a2adbada0e70858d8fb41dbba17dccb64c6
Aditya010102 <adityatiw9525@gmail.com>
2026-07-28 15:47:15 +0530

    change the seed_admin file for compatibility

--- backend/seed_admin.py
+++ backend/seed_admin.py
@@ -2,20 +2,23 @@ from app import app
 from extensions import db
 from models.user_model import User
 from werkzeug.security import generate_password_hash
+from getpass import getpass
 
 with app.app_context():
 
-    admin = User.query.filter_by(
-        email="admin@syntaxquiz.com"
-    ).first()
+    email = input("Enter admin email: ")
+    full_name = input("Enter admin name: ")
+    password = getpass("Enter admin password: ")
+
+    admin = User.query.filter_by(email=email).first()
 
     if admin:
         print("Admin already exists.")
     else:
         admin = User(
-            full_name="Administrator",
-            email="youradminpassword@gmail.com",
-            password=generate_password_hash("yourpassword@123"),
+            full_name=full_name,
+            email=email,
+            password=generate_password_hash(password),
             role="admin",
             provider="local"
         )
